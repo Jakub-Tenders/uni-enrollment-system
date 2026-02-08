93951acfb15e8438f51b983b9f250ae9824ca561
lostmart <martinpedraza1979@gmail.com>
2026-02-08 11:25:41 +0100

    startng backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 
 app.get("/api", (req, res) => {
-	res.json({ message: "Hello from the backend!" })
+	res.json({ message: "api goes here ..." })
 })
 
 /* health check  */
@@ -14,7 +14,9 @@ app.get("/health", (req, res) => {
 	res.status(200).json({ status: "OK", timestamp: new Date().toISOString() })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-	console.log(`Server is running. Test it out at http://localhost:${PORT}/health`)
+	console.log(
+		`Server is running. Test it out at http://localhost:${PORT}/health`,
+	)
 })
