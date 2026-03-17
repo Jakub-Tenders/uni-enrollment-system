675332f3be4418f707423ddd80639d0554dcd033
Jakub-Tenders <jmotysia@gmail.com>
2026-03-17 11:54:32 +0100

    Added all types

--- index.ts
+++ index.ts
@@ -1,4 +1,28 @@
+import { EphemeralKeyInfo } from "node:tls"
 import { v4 as uuidv4 } from "uuid"
+
+type Student = {
+    id : StudentId
+    name : string
+    email : Email
+    enrolledCredits : Credits
+}
+
+type Course = {
+    code : CourseCode
+    name : string
+    credits : Credits
+    capacity : number
+    enrolledCount : number
+}
+
+type Enrollment = {
+    id : EnrollmentId
+    studentid : StudentId
+    coursecode : CourseCode
+    semester : Semester
+    status: active|cancelled
+}
 
 const id = uuidv4()
 console.log("running the application ... ", id)
